408cfbe80d340499c2d6e8b482fa66f171afa1a1
Diego Rasero <diegolo.r8@gmail.com>
2013-02-28 21:17:17 +0100

    "Modificaciónfuncióndiscutirparaquedevuelva1encasodepoderaplicarcramer"

--- matrices.py
+++ matrices.py
@@ -17,30 +17,30 @@ def error(n):
 
 def x(M):
     """Devuelve el número de filas"""
-    return M[1][0] # devuelve m
+    return M[1][0]
 
 def y(M):
     """Devuelve el número de columnas"""
-    return M[1][1] # devuelve n
+    return M[1][1]
 
-def fila(M,f):
+def fila(M, f):
     """Devuelve la fila f de la matriz M"""
-    if f <= x(M) and f >= 1:
+    if f<=x(M) and f>=1:
         return M[0][(f-1)*y(M):((f-1)*y(M))+y(M)]
 
-def columna(M,c):
+def columna(M, c):
     """Devuelve la columna c de la matriz M"""
-    if c <= y(M) and c >= 1:
+    if c<=y(M) and c>=1:
         A=[]
-        for i in range(1,x(M)+1):
-            l=fila(M,i)
+        for i in range(1, x(M)+1):
+            l=fila(M, i)
             A=A+[l[c-1]]
         return A
 
 def comprueba(M):
     """comprueba la validez de una matriz"""
     try:
-        if len(M[0]) == x(M)*y(M) and len(M[1]) == 2:
+        if len(M[0])==x(M)*y(M) and len(M[1])==2:
             return M
         else:
             error(1)
@@ -49,7 +49,7 @@ def comprueba(M):
 
 def cuadrada(M):
     """Verifica que se trata de una matriz cuadrada"""
-    if x(M) == y(M):
+    if x(M)==y(M):
         return True
     else:
         return False
@@ -58,7 +58,7 @@ def diagonalprincipal(M):
     """ Devuelve la diagonal principal de una matriz"""
     if cuadrada(M):
         D=[]
-        for i in range(0,(x(M)*y(M)),(x(M)+1)):
+        for i in range(0, (x(M)*y(M)), (x(M)+1)):
             D.append(M[0][i])
         return D
 
@@ -66,10 +66,10 @@ def diagonalsecundaria(M):
     """ Devuelve la diagonal secundaria de una matriz"""
     if cuadrada(M):
         D=[]
-        if x(M) == 1:
+        if x(M)==1:
             D=[[M[0][0]]]
         else:
-            for i in range((y(M)-1),(y(M)*(y(M)-1))+1,(y(M)-1)):
+            for i in range((y(M)-1), (y(M)*(y(M)-1))+1, (y(M)-1)):
                 D.append(M[0][i])
         return D
 
@@ -78,9 +78,9 @@ def simetrica(M):
     if cuadrada(M):
         num=0
         for i in range(y(M)):
-            if fila(M,i)==columna(M,i):
+            if fila(M, i)==columna(M, i):
                 num=num+1
-        if num == y(M):
+        if num==y(M):
             return True
         else:
             return False
@@ -90,8 +90,8 @@ def simetrica(M):
 def Mlistasfilas(M):
     """Cambia el formato de las matrices a una lista con sublistas del tamaño de las columnas"""
     A=[]
-    for f in range(1,x(M)+1):
-        A.append(fila(M,f))
+    for f in range(1, x(M)+1):
+        A.append(fila(M, f))
     return A
 
 def Mlistadoble(M):
@@ -99,33 +99,33 @@ def Mlistadoble(M):
     A=[]
     for i in range(len(M)):
         A+=M[i]
-    return [A,[len(M),len(M[0])]]
+    return [A, [len(M), len(M[0])]]
 
 def imprime(M):
     """Imprime la matriz de forma ordenada"""
     string=""
     print("")
-    for f in range(1,x(M)+1): # rango numero de filas
-        for n in fila(M,f):
-            if n < 10:
+    for f in range(1, x(M)+1):
+        for n in fila(M, f):
+            if n<10:
                 string=string+"   "+str(n)
-            elif n < 100:
+            elif n<100:
                 string=string+"  "+str(n)
             else:
                 string=string+" "+str(n)
         print(string)
-        string =""
+        string=""
     print("")
 
 def Mnula(x, y):
     """ Crea una matriz nula"""
-    A = [0]*x*y
-    return [A,[x,y]]
+    A=[0]*x*y
+    return [A, [x, y]]
 
 def Midentidad(x):
     """ Crea una matriz identidad"""
-    A=Mnula(x,x)
-    for i in range(0,(x*x),(x+1)):
+    A=Mnula(x, x)
+    for i in range(0, (x*x), (x+1)):
         A[0][i]=1
     return A
 
@@ -133,56 +133,56 @@ def Maleatoria():
     """Crea una matriz de dimensiones aleatorias con un numeros aleatorios"""
     pass
 
-def suma(M1,M2):
+def suma(M1, M2):
     """Suma de dos matrices de las mismas dimensiones"""
     if y(M1)==y(M2) and x(M1)==x(M2):
         A=[]
         for i in range(y(M1)*x(M1)):
             A=A+[M1[0][i]+M2[0][i]]
-        return [A,[y(M1),x(M1)]]
+        return [A, [y(M1), x(M1)]]
 
-def resta(M1,M2):
+def resta(M1, M2):
     """Resta de dos matrices de las mismas dimensiones"""
     if y(M1)==y(M2) and x(M1)==x(M2):
         A=[]
         for i in range(y(M1)*x(M1)):
             A=A+[M1[0][i]-M2[0][i]]
-        return [A,[y(M1),x(M1)]]
+        return [A, [y(M1), x(M1)]]
 
-def prodnr(M,k):
+def prodnr(M, k):
     """Producto de un número real por una matriz"""
     A=[]
     for i in range(y(M)*x(M)):
         A=A+[M[0][i]*k]
-    return [A,[y(M),x(M)]]
+    return [A, [y(M), x(M)]]
 
-def producto(M1,M2):
+def producto(M1, M2):
     """Producto de dos matrices"""
-    def mys(l1,l2):
+    def mys(l1, l2):
         """Multiplica dos listas iguales y suma el resultado"""
         if len(l1)==len(l2):
             n=0
             for i in range(len(l1)):
                 n+=l1[i]*l2[i]
             return n
-    
-    if y(M1) == x(M2):
-        A=[[],[x(M1),y(M2)]]
-        for f in range(1,x(M1)+1):
-            for c in range(1,y(M2)+1):
-                A[0].append(mys(fila(M1,f),columna(M2,c)))
+
+    if y(M1)==x(M2):
+        A=[[], [x(M1), y(M2)]]
+        for f in range(1, x(M1)+1):
+            for c in range(1, y(M2)+1):
+                A[0].append(mys(fila(M1, f), columna(M2, c)))
         return A
     else:
         return error(1)
 
-def potencia(M,n):
+def potencia(M, n):
     """Potencia de una matriz"""
     if n==1:
         return M
     else:
         A=M
-        for i in range(2,n+1):
-            A=producto(M,A)
+        for i in range(2, n+1):
+            A=producto(M, A)
         return A
 
 def det(M):
@@ -193,90 +193,90 @@ def det(M):
         elif y(M)==2:
             return (M[0][0]*M[0][3])-(M[0][2]*M[0][1])
         elif y(M)==3:
-            return ( (M[0][0]*M[0][4]*M[0][8]) + (M[0][2]*M[0][3]*M[0][7]) + (M[0][1]*M[0][5]*M[0][6]) ) - ( (M[0][2]*M[0][4]*M[0][6]) + (M[0][1]*M[0][3]*M[0][8]) + (M[0][0]*M[0][5]*M[0][7]) )
+            return ((M[0][0]*M[0][4]*M[0][8])+(M[0][2]*M[0][3]*M[0][7])+(M[0][1]*M[0][5]*M[0][6]))-((M[0][2]*M[0][4]*M[0][6])+(M[0][1]*M[0][3]*M[0][8])+(M[0][0]*M[0][5]*M[0][7]))
         else:
-            f1=fila(M,1)
+            f1=fila(M, 1)
             deter=0
             for n in range(y(M)):
                 subM=[]
-                for nfila in range(2,y(M)+1):
-                    fi=fila(M,nfila)
+                for nfila in range(2, y(M)+1):
+                    fi=fila(M, nfila)
                     del fi[n]
                     subM+=fi
-                deter+= f1[n] * ((-1)**n) * det([subM,[y(M)-1,y(M)-1]])
+                deter+=f1[n]*((-1)**n)*det([subM, [y(M)-1, y(M)-1]])
             return deter
 
 def traspuesta(M):
     """Devuelve la matriz traspuesta a M"""
     A=[]
-    for col in range(1,y(M)+1):
-        A+=columna(M,col)
-    return [A,[y(M),x(M)]]
+    for col in range(1, y(M)+1):
+        A+=columna(M, col)
+    return [A, [y(M), x(M)]]
 
 def matrizAdjunta(M):
     """Calcula la matriz adjunta de M"""
     if not cuadrada(M):
         raise NameError('La matriz no es cuadrada')
-    if x(M) == 1:
+    if x(M)==1:
         return M[:]
-    if x(M) == 2:
-        A = [ [ M[0][3], -M[0][2], -M[0][1], M[0][0] ], [2, 2] ]
+    if x(M)==2:
+        A=[ [ M[0][3],-M[0][2],-M[0][1], M[0][0] ], [2, 2] ]
         return A
-    A = Mnula(x(M), y(M))
-    for i in range(1, x(M) + 1):
-        for j in range(1, y(M) + 1):
-            subM = submatriz(M, i, j)
-            A[0][(i - 1) * y(M) + j - 1] = (-1)**(i + j) * det(subM)
+    A=Mnula(x(M), y(M))
+    for i in range(1, x(M)+1):
+        for j in range(1, y(M)+1):
+            subM=submatriz(M, i, j)
+            A[0][(i-1)*y(M)+j-1]=(-1)**(i+j)*det(subM)
     return A
 
 def inversa(M):
     """Devuelve la matriz inversa a M"""
     if cuadrada(M):
         detm=det(M)
-        if detm != 0:
+        if detm!=0:
             Madj=matrizAdjunta(M)
-            return traspuesta(prodnr(Madj,(fractions.Fraction(1 , detm))))
+            return traspuesta(prodnr(Madj, (fractions.Fraction(1 , detm))))
         else:
             return error(3)
     else:
-        return error(2),error(3)
+        return error(2), error(3)
 
 def submatriz(M, f, c):
     """Elimina la fila f y la columna c de una matriz (menor complementario)"""
-    if x(M) < 2:
+    if x(M)<2:
         raise NameError("No puedo eliminar la única fila de la matriz")
-    if y(M) < 2:
+    if y(M)<2:
         raise NameError("No puedo eliminar la única columna de la matriz")
-    A = [M[0][:], M[1][:]]
-    del A[0][(f - 1) * y(A): f * y(A)]
-    for i in range(len(A[0]) - y(A) + c, 0, -y(A)):
-        del A[0][i - 1]
-    A[1][0] = A[1][0] - 1
-    A[1][1] = A[1][1] - 1
+    A=[M[0][:], M[1][:]]
+    del A[0][(f-1)*y(A): f*y(A)]
+    for i in range(len(A[0])-y(A)+c, 0,-y(A)):
+        del A[0][i-1]
+    A[1][0]=A[1][0]-1
+    A[1][1]=A[1][1]-1
     return A
 
 def rango(M):
-    A = Mlistasfilas(escalonar(M))
-    ceros = [0] * len(A[0])
-    return len(A) - A.count(ceros)
+    A=Mlistasfilas(escalonar(M))
+    ceros=[0]*len(A[0])
+    return len(A)-A.count(ceros)
 
-def cramer(A,S): # A = matriz 
+def cramer(A, S):
     """Resuelve sistemas empleando el metodo de cramer"""
-    def Mmodcramer(A,S,i):
-        csol=columna(S,1)
+    def Mmodcramer(A, S, i):
+        csol=columna(S, 1)
         B=[]
         for n in range(x(A)):
-            fA=fila(A,n+1)
+            fA=fila(A, n+1)
             fA[i]=csol[n]
             B+=fA
-        return [B,[x(A),x(A)]]
+        return [B, [x(A), x(A)]]
     da=det(A)
-    if da != 0:
+    if da!=0:
         sol=[]
-        for i in range(x(A)): #bucle con el numero de incognitas
-            xn=det(Mmodcramer(A,S,i))/da
+        for i in range(x(A)):
+            xn=det(Mmodcramer(A, S, i))/da
             sol.append(xn)
-        return sol # devuelve una lista con las soluciones en orden
+        return sol
     else:
         raise NameError(error(4))
 
@@ -285,57 +285,60 @@ def escalonar(M):
     #Primero un par de funciones auxiliares
     def simplifica(M):
         for fila in M:
-            mcd = 1
-            for i in range(2, min([abs(e) for e in fila]) + 1):
-                divisor = True
+            mcd=1
+            for i in range(2, min([abs(e) for e in fila])+1):
+                divisor=True
                 for e in fila:
-                    if e % i != 0:
-                        divisor = False
+                    if e%i!=0:
+                        divisor=False
                 if divisor:
-                    mcd = i
+                    mcd=i
             for i in range(0, len(fila)):
-                fila[i] = fila[i] / mcd
-                
+                fila[i]=fila[i]/mcd
+
     def reorganiza_pivote(M, f, c):
-        for i in range(f + 1, len(M)):
-            if M[i][c] != 0:
-                M[f], M[i] = M[i], M[f]
+        for i in range(f+1, len(M)):
+            if M[i][c]!=0:
+                M[f], M[i]=M[i], M[f]
                 break
-                
-    A = Mlistasfilas(M)
-    escalon = -1
+
+    A=Mlistasfilas(M)
+    escalon=-1
     for fila in range(0, len(A)):
         simplifica(A)
-        escalon = escalon + 1
-        if escalon >= len(A[0]): # Se nos agotan las columnas
+        escalon=escalon+1
+        if escalon>=len(A[0]): # Se nos agotan las columnas
             break
-        while A[fila][escalon] == 0: # Tenemos un cero de pivote. Hay que intercambiar filas
+        while A[fila][escalon]==0: # Tenemos un cero de pivote. Hay que intercambiar filas
             reorganiza_pivote(A, fila, escalon)
-            if A[fila][escalon] == 0: # Si no hay manera, corremos el escalon
-                escalon = escalon + 1
-                if escalon >= len(A[0]):
+            if A[fila][escalon]==0: # Si no hay manera, corremos el escalon
+                escalon=escalon+1
+                if escalon>=len(A[0]):
                     break
-        if escalon >= len(A[0]):
+        if escalon>=len(A[0]):
             break
-        for fila2 in range(fila + 1, len(A)):
-            p1, p2 = A[fila2][escalon], A[fila][escalon]
+        for fila2 in range(fila+1, len(A)):
+            p1, p2=A[fila2][escalon], A[fila][escalon]
             for c in range(escalon, len(A[0])):
-                A[fila2][c] = A[fila][c] * p1 - A[fila2][c] * p2
-    return Mlistadoble(A) 
+                A[fila2][c]=A[fila][c]*p1-A[fila2][c]*p2
+    return Mlistadoble(A)
 
-def Ampliada(A,S):
+def Ampliada(A, S):
     """Crea una matriz ampliada a partir de dos matrices con el mismo número de filas"""
     M=[]
-    for f in range(1,x(A)+1):
-        M+=(fila(A,f)+fila(S,f))
-    return [M,[x(A),(y(A)+y(S))]]
+    for f in range(1, x(A)+1):
+        M+=(fila(A, f)+fila(S, f))
+    return [M, [x(A), (y(A)+y(S))]]
 
-def discutir(A,S): 
+def discutir(A, S):
     rgA=rango(A)
-    if rgA != rango(Ampliada(A,S)):
+    if rgA!=rango(Ampliada(A, S)):
         print("Sistema incompatible (sin solución)")
+        return 0
     else: # falta hacer que prescinda de ecuaciones para luego aplicar cramer 
-        if rgA == len(fila(A,1)): # si el rango es igual al numero de incognitas
+        if rgA==len(fila(A, 1)):
             print("Sistema compatible determidado (solución unica)")
-        elif rgA < len(fila(A,1)): 
+            return 1
+        elif rgA<len(fila(A, 1)):
             print("Sistema compatible indeterminado (infinitas soluciones)")
+            return 2
